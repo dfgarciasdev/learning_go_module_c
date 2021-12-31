bc3f0bdd4591dbb9b353d5260a82db2fe577133f
davidgarcia <dfgarcias.dev@gmail.com>
2021-12-30 19:04:50 -0500

    :bug: Used new version operations

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/dfgarciasdev/learning_go_module_c
 
 go 1.17
 
-require github.com/dfgarciasdev/learning_go_module_a v1.0.0 
+require( 
+github.com/dfgarciasdev/learning_go_module_a v1.0.0
+
+github.com/dfgarciasdev/learning_go_module_a/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	operations "github.com/dfgarciasdev/learning_go_module_a"
+	operationsV2 "github.com/dfgarciasdev/learning_go_module_a/v2"
 )
 
 func main() {
 	values := operations.Values{A: 1, B: 2}
 	fmt.Println(values.Sum())
+	fmt.Println(operationsV2.Sum(values.A, values.B))
 }
